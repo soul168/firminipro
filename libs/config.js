bf2ac3fec7d810cede04a3a218b83d5b325d4c1f
soul <1429159476@qq.com>
2020-10-16 16:46:59 +0800

    提交
    提交

---
+++ libs/config.js
@@ -0,0 +1,8 @@
+// https://openplatform.starcart.cn  https://weixinjiang.starcart.cn
+var config = {
+  key: '86653227c60add814b33098f58fea9c6',
+  servPath: 'https://openplatform.starcart.cn',
+  openapi: 'https://openapi.starcart.cn'
+}
+
+module.exports.Config = config;
